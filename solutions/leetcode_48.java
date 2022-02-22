5f6ccab41f4b2bdafb703886ded71416907a9228
Anjanay Singh <63287310+Dras-07@users.noreply.github.com>
2022-02-22 15:50:33 +0530

    Create leetcode_48.java

---
+++ solutions/leetcode_48.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n=matrix.length;
+      int ans[][]=new int[n][n];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               ans[j][i]=matrix[n-1-i][j];
+            }
+        }
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                matrix[i][j]=ans[i][j];
+        
+    }
+}
