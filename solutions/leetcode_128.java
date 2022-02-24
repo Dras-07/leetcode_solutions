fec9de1db6affcb0d7b3003a4ef7f8ebd95c55bf
Anjanay Singh <63287310+Dras-07@users.noreply.github.com>
2022-02-25 00:30:13 +0530

    Update leetcode_128.java

--- solutions/leetcode_128.java
+++ solutions/leetcode_128.java
@@ -1,3 +1,4 @@
+//method1
 class Solution {
     public int longestConsecutive(int[] nums) {
         
@@ -32,5 +33,52 @@ class Solution {
         }
         m=Math.max(m,c);
         return m;
+    }
+}
+//Method 2
+
+class Solution {
+    public int solve(HashSet<Integer> hm,int x)
+    {
+        int c=1;
+        for(int i=x+1;i<=100000;i++)
+        {
+            if(hm.contains(i))
+                c++;
+            else
+                return c;
+        }
+        return c;
+    }
+    public int longestConsecutive(int[] nums) {
+        
+        
+        HashMap<Integer,Boolean> hm=new HashMap<>();
+        HashSet<Integer> hm1=new HashSet<>();
+        for(int i:nums)
+        {
+            hm1.add(i);
+        }
+        for(int i:nums)
+        {
+            if(hm1.contains(i-1))
+            {
+                hm.put(i,false);
+            }
+            else
+            {
+                hm.put(i,true);
+            }
+        }
+        int max=0;
+        for(Integer x:hm.keySet())
+        {
+            if(hm.get(x)==true)
+            {
+                int d=solve(hm1,x);
+                max=Math.max(d,max);
+            }
+        }
+        return max;
     }
 }
