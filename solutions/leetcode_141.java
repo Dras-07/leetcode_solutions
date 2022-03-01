d175c7059d9f80a6e69de62981c26824f3afa0c5
Anjanay Singh <63287310+Dras-07@users.noreply.github.com>
2022-03-01 18:39:55 +0530

    Create leetcode_141.java

---
+++ solutions/leetcode_141.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if(head==null)
+            return false;
+        if(head.next==null)
+            return false;
+        
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
+}
